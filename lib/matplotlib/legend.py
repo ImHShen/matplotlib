1c377c980f0c67d73a9118489807706f9a20a7cd
DanHickstein <danhickstein@gmail.com>
2020-01-09 08:52:41 -0700

    legend textcolor: removed trailing whitespace

--- lib/matplotlib/legend.py
+++ lib/matplotlib/legend.py
@@ -176,7 +176,7 @@ fontsize : int or {'xx-small', 'x-small', 'small', 'medium', 'large', \
 textcolor : str or list
     Sets the color of the text in the legend. Can be a valid color string
     (for example, 'red'), or a list of color strings. The textcolor can
-    also be made to match the color of the line or marker using 'linecolor', 
+    also be made to match the color of the line or marker using 'linecolor',
     'markerfacecolor' (or 'mfc'), or 'markeredgecolor' (or 'mec').
 
 numpoints : int, default: :rc:`legend.numpoints`
